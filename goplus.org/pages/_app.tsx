0e9c1d0e833c6731841a443620a8aab6e5ad1bc3
Hanxing Yang <nighca@live.cn>
2021-10-12 11:04:41 +0800

    Initialize with Next.js & Qiniu Information (#1)
    * initialize with next.js
    
    * content for GoPlus (#1)

---
+++ goplus.org/pages/_app.tsx
@@ -0,0 +1,7 @@
+import './global.css'
+import type { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+export default MyApp
