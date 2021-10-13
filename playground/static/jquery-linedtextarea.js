4f35fc5c5a842412ae96966e740977aa73578744
qiukeren <qiukeren@qiniu.com>
2021-10-13 17:14:45 +0800

    add mobile mode for play.goplus.org

--- playground/static/jquery-linedtextarea.js
+++ playground/static/jquery-linedtextarea.js
@@ -1,7 +1,7 @@
 /**
  * Adapted from jQuery Lined Textarea Plugin
  * http://alan.blog-city.com/jquerylinedtextarea.htm
- * 
+ *
  * Released under the MIT License:
  * http://www.opensource.org/licenses/mit-license.php
  */
@@ -23,10 +23,18 @@
 			var lineNo = 1;
 			var textarea = $(this);
 
+			var width = $(document).width();
+			if (width > 1024){
 			/* Wrap the text area in the elements we need */
-			textarea.wrap("<div class='linedtextarea' style='height:100%; overflow:hidden'></div>");
-			textarea.width("97%");
-			textarea.parent().prepend("<div class='lines' style='width:3%'></div>");
+			    textarea.wrap("<div class='linedtextarea' style='height:100%; overflow:hidden'></div>");
+			    textarea.width("97%");
+			    textarea.parent().prepend("<div class='lines' style='width:3%'></div>");
+			}else{
+				/* Wrap the text area in the elements we need */
+				textarea.wrap("<div class='linedtextarea' style='height:100%; overflow:hidden'></div>");
+				textarea.width("95%");
+				textarea.parent().prepend("<div class='lines' style='width:5%'></div>");
+			}
 			var linesDiv = textarea.parent().find(".lines");
 
 			var scroll = function(tn) {
@@ -48,3 +56,4 @@
 	};
 
 })(jQuery);
+
