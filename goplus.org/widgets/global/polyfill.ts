453a769aaca4eeb91e48a56342f5648433d9e0f1
nighca <nighca@live.cn>
2022-01-22 14:06:19 +0800

    fix build

--- goplus.org/widgets/global/polyfill.ts
+++ goplus.org/widgets/global/polyfill.ts
@@ -3,5 +3,5 @@
  * @desc Not for browsers support
  */
 
-window.process = window.process || { env: {} };
+window.process = window.process || { env: {} } as any;
 (window.process.env as any)['NODE_ENV'] = process.env.NODE_ENV
