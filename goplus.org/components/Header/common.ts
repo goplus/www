76a1cd4520895cee53bb61c9ea54a969ae0eec07
uiu / 我想养只猫 •͓͡•ʔ <uiuing@foxmail.com>
2022-05-26 15:57:56 +0800

    Optimize <Logo> tag and <a> tag attributes (#61)
    Signed-off-by: uiuing <uiuing@foxmail.com>

--- goplus.org/components/Header/common.ts
+++ goplus.org/components/Header/common.ts
@@ -3,27 +3,33 @@ import { ReactNode } from 'react'
 export interface NavItemInfo {
   children: ReactNode
   href: string
+  isBlank: boolean
 }
 
 export const navItems: NavItemInfo[] = [
   {
     children: 'Overview',
-    href: 'https://goplus.org'
+    href: 'https://goplus.org',
+    isBlank: false
   },
   {
     children: 'Tutorials',
-    href: 'https://tutorial.goplus.org'
+    href: 'https://tutorial.goplus.org',
+    isBlank: false
   },
   {
     children: 'Playground',
-    href: 'https://play.goplus.org/'
+    href: 'https://play.goplus.org/',
+    isBlank: true
   },
   {
     children: 'Contributing',
-    href: 'https://github.com/goplus/gop#contributing'
+    href: 'https://github.com/goplus/gop#contributing',
+    isBlank: true
   },
   {
     children: 'IDE Plugins',
-    href: 'https://github.com/goplus/gop#ide-plugins'
+    href: 'https://github.com/goplus/gop#ide-plugins',
+    isBlank: true
   }
 ]
