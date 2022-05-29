5219a302979fbe9d01a8c65fe72d5e341b5f05b3
uiu / 我想养只猫 •͓͡•ʔ <uiuing@foxmail.com>
2022-05-29 22:31:36 +0800

    Remove the "-" at the end of the anchor ID in the title. (#62)
    Signed-off-by: uiuing <uiuing@foxmail.com>

--- goplus.org/components/Home/Features/common.ts
+++ goplus.org/components/Home/Features/common.ts
@@ -2,6 +2,7 @@
 export function getAnchorId(title: string) {
   return title
     .split(/[^\w\d\s]/, 1)[0]
+    .trim()
     .split(/\s+/)
     .map(word => word.toLocaleLowerCase())
     .join('-')
