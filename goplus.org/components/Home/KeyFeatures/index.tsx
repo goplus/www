691a4207734e4baf8ad19c52f25c28dee1a48fe0
nighca <nighca@live.cn>
2024-02-23 10:27:43 +0800

    fix broken links in homepage

--- goplus.org/components/Home/KeyFeatures/index.tsx
+++ goplus.org/components/Home/KeyFeatures/index.tsx
@@ -13,7 +13,7 @@ const content = `
 * The simplest engineering language that can be mastered by children (script-like style).
 * Performance: as fast as Go (Go+'s main backend compiles to human-readable Go).
 * Fully compatible with [Go](https://github.com/golang/go) and can mix Go/Go+ code in the same package (see [Go/Go+ hybrid programming](https://github.com/goplus/gop/blob/main/doc/docs.md#gogo-hybrid-programming)).
-* No DSL (Domain Specific Language) support, but it's Specific Domain Friendly (see [DSL vs. SDF](https://github.com/goplus/gop/blob/main/doc/dsl-vs-sdf.md)).
+* No DSL (Domain Specific Language) support, but SDF ([Specific Domain Friendliness](https://github.com/goplus/gop/blob/main/doc/classfile.md)).
 * Support Go code generation (main backend) and [bytecode backend](https://github.com/goplus/igop) (REPL: see [iGo+](https://repl.goplus.org/)).
 * [Simplest way to interaction with C](https://github.com/goplus/gop/blob/main/doc/docs.md#calling-c-from-go) (cgo is supported but not recommended).
 * [Powerful built-in data processing capabilities](https://github.com/goplus/gop/blob/main/doc/docs.md#data-processing).
