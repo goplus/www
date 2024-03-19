fc1090fc4589104e0233c0523e92bca2cd11b9f0
JaamieZheng <2944334243@qq.com>
2024-03-19 20:21:41 +0800

    fix build bugs

--- goplus.org/pages/Download.tsx
+++ goplus.org/pages/Download.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, HTMLAttributes, SVGProps } from 'react';
+import { NextApiRequest, NextApiResponse } from 'next';
 import Head from 'next/head';
 import styles from 'pages/style.module.scss';
 import Layout from 'components/Layout';
@@ -7,8 +8,86 @@ import ButtonUpLogo from 'components/Icon/ButtonUpLogo'
 // import { downloadTable as DownloadTableComponent } from 'components/DownloadTable';
 // import GithubReleases, { GithubStableReleases } from './download/GithubReleases';
 
+interface Author {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+}
 
-function DownloadTable({ browser_download_url, name, Size }) {
+interface Uploader {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+}
+
+interface Asset {
+  url: string,
+  id: number,
+  node_id: string,
+  name: string,
+  label: string,
+  uploader: Uploader,
+  content_type: string,
+  state: string,
+  size: number,
+  download_count: number,
+  created_at: string,
+  updated_at: string,
+  browser_download_url: string
+}
+
+interface GitHubRelease {
+  url: string;
+  assets_url: string;
+  upload_url: string;
+  html_url: string;
+  id: number;
+  author: Author;
+  node_id: string;
+  tag_name: string;
+  target_commitish: string;
+  name: string;
+  draft: boolean;
+  prerelease: boolean;
+  created_at: string;
+  published_at: string;
+  assets: Asset[];
+}
+
+
+
+
+function DownloadTable({ browser_download_url, name, Size }: { browser_download_url: string, name: string, Size: number }) {
   let fillContent = '', os = '', Arch = '', size_str = '';
   // Size = Size / 1024
   if (Size < 1024) {
@@ -18,7 +97,7 @@ function DownloadTable({ browser_download_url, name, Size }) {
   } else if (Size < (1024 * 1024 * 1024)) {
     size_str = ((Size / (1024 * 1024)).toFixed(2)).toString() + 'MB';
   } else {
-    size_str = ((Size / (1024 * 1024 * 1024).toFixed(2))).toString() + 'GB';
+    size_str = ((Size / (1024 * 1024 * 1024)).toFixed(2)).toString() + 'GB';
   }
 
 
@@ -52,7 +131,7 @@ function DownloadTable({ browser_download_url, name, Size }) {
 
 
 
-  return (<tr className={styles.table_hightlight}>
+  return (<>
     <td className={styles.filename}>
       <a className={styles.link} href={browser_download_url}>
         {name}
@@ -74,34 +153,34 @@ function DownloadTable({ browser_download_url, name, Size }) {
       {Checksum}
     </td> */}
 
-  </tr>
+  </>
   )
 }
 
 
 
 
-export default function Home({ StableRelease, ArchivedReleases }) {
+export default function Home({ StableRelease, ArchivedReleases }: { StableRelease: GitHubRelease, ArchivedReleases: GitHubRelease[] }) {
 
-  const [selectedRelease, setSelectedRelease] = useState(null);
+  const [selectedRelease, setSelectedRelease] = useState(-1);
   const [isContentVisible, setContentVisible] = useState(true);
 
   const toggleContentVisibility = () => {
     setContentVisible(!isContentVisible);
   };
 
-  const toggleReleaseAssets = (release) => {
-    if (selectedRelease === release) {
-      setSelectedRelease(null);
+  const toggleReleaseAssets = (release: GitHubRelease) => {
+    if (selectedRelease === release.id) {
+      setSelectedRelease(-1);
     } else {
-      setSelectedRelease(release);
+      setSelectedRelease(release.id);
     }
   };
 
   const latestWinAsset = StableRelease.assets.filter(asset => ((asset.name).includes("win") || (asset.name).includes("Win")) && (asset.name).includes("x86_64") && (asset.name).includes(".zip"))[0]
   const latestIOSx86Asset = StableRelease.assets.filter(asset => ((asset.name).includes("darwin") || (asset.name).includes("Darwin")) && (asset.name).includes("x86_64") && (asset.name).includes(".tar.gz"))[0]
   const latestIOSarmAsset = StableRelease.assets.filter(asset => ((asset.name).includes("darwin") || (asset.name).includes("Darwin")) && (asset.name).includes("arm") && (asset.name).includes(".tar.gz"))[0]
-  const latestLinuxarmAsset = StableRelease.assets.filter(asset =>( (asset.name).includes("linux") || (asset.name).includes("Linux")) && (asset.name).includes("arm") && (asset.name).includes(".tar.gz"))[0]
+  const latestLinuxarmAsset = StableRelease.assets.filter(asset => ((asset.name).includes("linux") || (asset.name).includes("Linux")) && (asset.name).includes("arm") && (asset.name).includes(".tar.gz"))[0]
 
   return (
     <Layout>
@@ -128,16 +207,16 @@ export default function Home({ StableRelease, ArchivedReleases }) {
             </a>
 
             <a href={latestIOSx86Asset.browser_download_url} className={styles.download_box}>
-                <h4 className={styles.download_box_title}>Apple MacOS (X86_64)</h4>
-                <p className={styles.download_box_p}>macOS 10.15 or later, Intel 64-bit processor</p>
-                <span  className={styles.download_box_link}>{latestIOSx86Asset.name}</span>
-              </a>
+              <h4 className={styles.download_box_title}>Apple MacOS (X86_64)</h4>
+              <p className={styles.download_box_p}>macOS 10.15 or later, Intel 64-bit processor</p>
+              <span className={styles.download_box_link}>{latestIOSx86Asset.name}</span>
+            </a>
 
-              <a href={latestIOSarmAsset.browser_download_url} className={styles.download_box}>
-                <h4 className={styles.download_box_title}>Apple MacOS (ARM64)</h4>
-                <p className={styles.download_box_p}>macOS 11 or later, Apple 64-bit processor</p>
-                <span className={styles.download_box_link}>{latestIOSarmAsset.name}</span>
-              </a>
+            <a href={latestIOSarmAsset.browser_download_url} className={styles.download_box}>
+              <h4 className={styles.download_box_title}>Apple MacOS (ARM64)</h4>
+              <p className={styles.download_box_p}>macOS 11 or later, Apple 64-bit processor</p>
+              <span className={styles.download_box_link}>{latestIOSarmAsset.name}</span>
+            </a>
 
             <a href={latestLinuxarmAsset.browser_download_url} className={styles.download_box}>
               <h4 className={styles.download_box_title}>Linux (ARM64)</h4>
@@ -157,10 +236,10 @@ export default function Home({ StableRelease, ArchivedReleases }) {
             <li key={StableRelease.id}>
               <div onClick={() => toggleReleaseAssets(StableRelease)} className={styles.release_item_name}>
                 <span>GoPlus_{StableRelease.tag_name}</span>
-                {selectedRelease != StableRelease && (<ButtonLogo />)}
-                {selectedRelease === StableRelease && (<ButtonUpLogo />)}
+                {selectedRelease != StableRelease.id && (<ButtonLogo />)}
+                {selectedRelease === StableRelease.id && (<ButtonUpLogo />)}
               </div>
-              {selectedRelease === StableRelease && (
+              {selectedRelease === StableRelease.id && (
                 <ul className={styles.release_list}>
                   <table className={styles.downloadTable}>
                     <thead className={styles.downloadTable_header}>
@@ -173,11 +252,15 @@ export default function Home({ StableRelease, ArchivedReleases }) {
                     </thead>
                     <tbody>
                       {StableRelease.assets.map((asset) => (
-                        <DownloadTable
-                          browser_download_url={asset.browser_download_url}
-                          name={asset.name}
-                          Size={asset.size}
-                        />
+                        < tr className={styles.table_hightlight} key={asset.id}>
+                          <DownloadTable
+
+                            browser_download_url={asset.browser_download_url}
+                            name={asset.name}
+                            Size={asset.size}
+                          />
+                        </tr>
+
                       ))}
                     </tbody>
                   </table>
@@ -198,10 +281,10 @@ export default function Home({ StableRelease, ArchivedReleases }) {
                   <li key={release.id}>
                     <div onClick={() => toggleReleaseAssets(release)} className={styles.release_item_name}>
                       <span>GoPlus_{release.tag_name}</span>
-                      {selectedRelease != release && (<ButtonLogo />)}
-                      {selectedRelease === release && (<ButtonUpLogo />)}
+                      {selectedRelease != release.id && (<ButtonLogo />)}
+                      {selectedRelease === release.id && (<ButtonUpLogo />)}
                     </div>
-                    {selectedRelease === release && (
+                    {selectedRelease === release.id && (
                       <ul className={styles.release_list}>
                         {release.assets.length === 0 && <span className={styles.release_span}>No content available now !</span>}
                         {release.assets.length != 0 && <table className={styles.downloadTable}>
@@ -214,12 +297,14 @@ export default function Home({ StableRelease, ArchivedReleases }) {
                             {/* <th>SHA256 Checksum</th> */}
                           </thead>
                           <tbody>
-                            {release.assets.map((asset) => (
+                            {release.assets.map((asset) => (< tr className={styles.table_hightlight} key={asset.id}>
                               <DownloadTable
+
                                 browser_download_url={asset.browser_download_url}
                                 name={asset.name}
                                 Size={asset.size}
                               />
+                            </tr>
                             ))}
                           </tbody>
                         </table>}
@@ -237,7 +322,7 @@ export default function Home({ StableRelease, ArchivedReleases }) {
   )
 }
 
-export async function getServerSideProps({ req, res }) {
+export async function getServerSideProps({ req, res }: { req: NextApiRequest, res: NextApiResponse }) {
   res.setHeader(
     'Cache-Control',
     'public, s-maxage=10, stale-while-revalidate=59'
@@ -246,7 +331,7 @@ export async function getServerSideProps({ req, res }) {
   const response = await fetch('https://api.github.com/repos/goplus/gop/releases');
 
   console.log("ok")
-  let data = await response.json();
+  let data: GitHubRelease[] = await response.json();
   const releases = data.filter(Release => !(Release.tag_name).includes("pre") && !(Release.tag_name).includes("beta") && !(Release.tag_name).includes("alpha") && !(Release.tag_name).includes("rc"))
   // Releases = data;
 
