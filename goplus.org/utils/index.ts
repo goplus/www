b274333f721a7f38d4557a3157e80e68e87f4c37
JaamieZheng <2944334243@qq.com>
2024-04-09 16:44:51 +0800

    demo1

--- goplus.org/utils/index.ts
+++ goplus.org/utils/index.ts
@@ -1,6 +1,5 @@
 import { Children, ReactNode, isValidElement } from 'react'
 import variables from './variables.module.scss'
-import { loadEnvConfig } from '@next/env'
 
 /** 判断当前是否在浏览器中执行，与之对应的是在 Node.js 环境执行（生成静态页面时） */
 export function isBrowser() {
@@ -38,9 +37,9 @@ export function getOrigin() {
   if (process.env.NODE_ENV === 'development') {
     return "http://localhost:3000"
   } else {
-    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview' && process.env.VERCEL_URL) {
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview' && process.env.VERCEL_BRANCH_URL) {
       // 在预览环境下，将 /download 路径重定向到预览页面
-      return 'https://' + process.env.VERCEL_URL
+      return 'https://' + process.env.VERCEL_BRANCH_URL
     } else {
       // 在其他环境下，保持 /download 路径不变
       return "https://goplus.org"
