8ab7d8cc21554f3785a4a0891f0c827ae9f9f6f1
luoliwoshang <2643523683@qq.com>
2025-03-18 19:24:22 +0800

    www:prevent AbortSignal type conflicts in widget builds

--- goplus.org/next.config.js
+++ goplus.org/next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const path = require("path")
-const CopyPlugin = require("copy-webpack-plugin")
+
+const getCopyPlugin = () => {
+  if (!process.env.COPY_ASSETS) {
+    return null
+  }
+  const CopyPlugin = require("copy-webpack-plugin")
+  return new CopyPlugin({
+    patterns: [
+      {
+        from: path.join(process.cwd(), 'articles/**/*.{jpg,jpeg,png,gif,svg}'),
+        to: path.join(process.cwd(), 'public'),
+      },
+    ],
+  })
+}
 
 module.exports = {
   reactStrictMode: true,
@@ -10,16 +24,13 @@ module.exports = {
       test: /\.(md|gop)$/,
       type: 'asset/source'
     })
-    config.plugins.push(
-      new CopyPlugin({
-        patterns: [
-          {
-            from: path.join(process.cwd(), 'articles/**/*.{jpg,jpeg,png,gif,svg}'),
-            to: path.join(process.cwd(), 'public'),
-          },
-        ],
-      }),
-    )
+    
+    // Conditionally require plugin to prevent AbortSignal type conflicts in widget builds
+    const copyPlugin = getCopyPlugin()
+    if (copyPlugin) {
+      config.plugins.push(copyPlugin)
+    }
+    
     return config
   },
 }
